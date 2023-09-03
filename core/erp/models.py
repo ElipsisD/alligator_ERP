dd4058b839914e73646c50c1b945b99497a2f3bc
Dmitry <da-boyko@mail.ru>
2023-09-03 20:46:36 +0700

    feat: add fields to User model

--- core/erp/models.py
+++ core/erp/models.py
@@ -6,18 +6,22 @@ from erp.enums import WorkArea
 
 
 class User(AbstractUser):
-    telegram_name = models.CharField(max_length=150)
+    chat_id = models.PositiveIntegerField(verbose_name='идентификатор чата')
+    telegram_username = models.CharField(max_length=150, verbose_name='username', blank=True, null=True)
+    area = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='место работы', blank=True, null=True)
 
     class Meta:
-        verbose_name = 'пользователь'
-        verbose_name_plural = 'пользователи'
+        verbose_name = 'сотрудник'
+        verbose_name_plural = 'сотрудники'
 
     def __str__(self):
         return self.get_full_name()
 
 
 class Transfer(models.Model):
-    author = models.ForeignKey(to='erp.User', on_delete=models.DO_NOTHING, related_name='transfers', verbose_name='автор')
+    author = models.ForeignKey(
+        to='erp.User', on_delete=models.DO_NOTHING, related_name='transfers', verbose_name='автор'
+    )
     date = models.DateTimeField(auto_now_add=True, verbose_name='дата и время')
     item_number = models.CharField(
         max_length=11, validators=[validators.MinLengthValidator(limit_value=11)], verbose_name='номенклатурный номер'
