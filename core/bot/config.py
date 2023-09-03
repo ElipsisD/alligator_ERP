75da5af0dc5c93e1a75fd1197dc88e2a9c172abc
Dmitry <da-boyko@mail.ru>
2023-09-03 20:49:49 +0700

    feat: add handlers to config

--- core/bot/config.py
+++ core/bot/config.py
@@ -3,3 +3,15 @@ from bot import handlers
 COMMAND_HANDLERS = {
     "start": handlers.start,
 }
+
+HANDLERS = {
+    handlers.echo: handlers.echo_filter,
+}
+
+CONVERSATION_HANDLERS = (
+    handlers.register_conv_handler,
+)
+
+CALLBACK_QUERY_HANDLERS = {
+    'check': handlers.check,
+}
