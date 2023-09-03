d8214c7ed6341b50d2822a8f618759c2a9cf720c
Dmitry <da-boyko@mail.ru>
2023-09-03 20:48:26 +0700

    feat: add new User model to admin panel

--- core/erp/admin.py
+++ core/erp/admin.py
@@ -14,5 +14,41 @@ class TransferAdmin(admin.ModelAdmin):
 @admin.register(User)
 class MyUserAdmin(UserAdmin):
 
+    list_display = (
+        'pk',
+        'last_name',
+        'first_name',
+        'telegram_username',
+        'chat_id',
+        'area',
+    )
+
+    fieldsets = (
+        (
+            'Персональная информация',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'area',
+                )
+            },
+        ),
+        (
+            'Telegram',
+            {
+                'fields': (
+                    'chat_id',
+                    'telegram_username',
+                )
+            }
+        )
+    )
+
+    readonly_fields = (
+        'chat_id',
+        'telegram_username',
+    )
+
     class Meta:
         model = User
