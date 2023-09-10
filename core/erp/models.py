a3ee9a0780f6dfd6fd6166a9e1b0bb5e3c50facf
Dmitry <da-boyko@mail.ru>
2023-09-10 19:23:43 +0700

    feat: add Production model

--- core/erp/models.py
+++ core/erp/models.py
@@ -37,3 +37,24 @@ class Transfer(models.Model):
 
     def __str__(self):
         return f'Перемещение №{self.pk}'
+
+
+class Production(models.Model):
+    author = models.ForeignKey(
+        to='erp.User', on_delete=models.DO_NOTHING, related_name='productions', verbose_name='автор'
+    )
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата и время')
+    item_number = models.CharField(
+        max_length=11, validators=[validators.MinLengthValidator(limit_value=11)], verbose_name='номенклатурный номер'
+    )
+    amount = models.PositiveSmallIntegerField(verbose_name='количество')
+    comment = models.TextField(verbose_name='комментарий', blank=True, null=True)
+    sender = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='отправитель')
+    receiver = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='получатель')
+
+    class Meta:
+        verbose_name = 'факт производства'
+        verbose_name_plural = 'факты производства'
+
+    def __str__(self):
+        return f'Факт производства №{self.pk}'
