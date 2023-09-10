3323b7fe8d5a166e1100cc17eacac1733dc12b3e
Dmitry <da-boyko@mail.ru>
2023-09-10 19:25:06 +0700

    feat: create area enum

--- core/erp/enums.py
+++ core/erp/enums.py
@@ -2,8 +2,14 @@ from django.db.models import TextChoices
 
 
 class WorkArea(TextChoices):
-    LASER = ("LASER", "Лазерный участок")
-    WELDING = ("WELDING", "Сварочный участок")
-    MOTOR = ("MOTOR", "Моторный участок")
-    ELECTRICIAN = ("ELECTRICIAN", "Участок электрики")
-    ASSEMBLY = ("ASSEMBLY", "Сборочный участок")
+    LASER = ('LASER', 'Лазерный участок')
+    WELDING = ('WELDING', 'Сварочный участок')
+    MOTOR = ('MOTOR', 'Моторный участок')
+    ELECTRICIAN = ('ELECTRICIAN', 'Участок электрики')
+    ASSEMBLY = ('ASSEMBLY', 'Сборочный участок')
+
+    @classmethod
+    def get_label_by_name(cls, value):
+        for name, label in cls.choices:
+            if name == value:
+                return label
