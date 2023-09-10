36dadbdef03311cdd33f56657b0130b84f884aa1
Dmitry <da-boyko@mail.ru>
2023-09-10 19:27:55 +0700

    feat: change start handler logic

--- core/bot/handlers/start.py
+++ core/bot/handlers/start.py
@@ -1,14 +1,26 @@
+from asgiref.sync import sync_to_async
 from telegram import Update, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
-from bot.keyboards import start_keyboard
+from bot.keyboards import start_keyboard, start_work_keyboard
+from erp.models import User
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.delete()
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Добрый день, сотрудник!\n\n"
-             "Чтобы пройти регистрацию, нажми на кнопку ниже!",
-        reply_markup=start_keyboard,
-    )
+    try:
+        user = await sync_to_async(User.objects.get)(chat_id=update.message.chat.id)
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Здравствуйте, {user.first_name}!\n\n"
+                 f"Какую информацию нужно внести?\n",
+            reply_markup=start_work_keyboard,
+        )
+
+    except User.DoesNotExist:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Добрый день, сотрудник!\n\n"
+                 "Чтобы пройти регистрацию, нажмите на кнопку ниже!",
+            reply_markup=start_keyboard,
+        )
