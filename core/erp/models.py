5ed00fa7185caf2dfec36f414352b5841db26766
Dmitry <da-boyko@mail.ru>
2023-09-17 12:49:07 +0700

    feat: change fields to models

--- core/erp/models.py
+++ core/erp/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core import validators
 from django.db import models
 
@@ -6,9 +7,19 @@ from erp.enums import WorkArea
 
 
 class User(AbstractUser):
-    chat_id = models.PositiveIntegerField(verbose_name='идентификатор чата')
+    chat_id = models.PositiveIntegerField(verbose_name='идентификатор чата', null=True)
+    patronymic = models.CharField(verbose_name="отчество", max_length=150, blank=True)
     telegram_username = models.CharField(max_length=150, verbose_name='username', blank=True, null=True)
     area = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='место работы', blank=True, null=True)
+    username = models.CharField(
+        verbose_name="username",
+        max_length=150,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[UnicodeUsernameValidator()],
+        error_messages={"unique": "A user with that username already exists."},
+        null=True,
+    )
 
     class Meta:
         verbose_name = 'сотрудник'
@@ -18,16 +29,22 @@ class User(AbstractUser):
         return self.get_full_name()
 
 
-class Transfer(models.Model):
-    author = models.ForeignKey(
-        to='erp.User', on_delete=models.DO_NOTHING, related_name='transfers', verbose_name='автор'
-    )
+class AbstractModel(models.Model):
     date = models.DateTimeField(auto_now_add=True, verbose_name='дата и время')
     item_number = models.CharField(
         max_length=11, validators=[validators.MinLengthValidator(limit_value=11)], verbose_name='номенклатурный номер'
     )
     amount = models.PositiveSmallIntegerField(verbose_name='количество')
     comment = models.TextField(verbose_name='комментарий', blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Transfer(AbstractModel):
+    author = models.ForeignKey(
+        to='erp.User', on_delete=models.CASCADE, related_name='transfers', verbose_name='автор'
+    )
     sender = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='отправитель')
     receiver = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='получатель')
 
@@ -39,18 +56,11 @@ class Transfer(models.Model):
         return f'Перемещение №{self.pk}'
 
 
-class Production(models.Model):
+class Production(AbstractModel):
     author = models.ForeignKey(
-        to='erp.User', on_delete=models.DO_NOTHING, related_name='productions', verbose_name='автор'
+        to='erp.User', on_delete=models.CASCADE, related_name='productions', verbose_name='автор'
     )
-    date = models.DateTimeField(auto_now_add=True, verbose_name='дата и время')
-    item_number = models.CharField(
-        max_length=11, validators=[validators.MinLengthValidator(limit_value=11)], verbose_name='номенклатурный номер'
-    )
-    amount = models.PositiveSmallIntegerField(verbose_name='количество')
-    comment = models.TextField(verbose_name='комментарий', blank=True, null=True)
-    sender = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='отправитель')
-    receiver = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='получатель')
+    location = models.CharField(choices=WorkArea.choices, max_length=11, verbose_name='цех производства')
 
     class Meta:
         verbose_name = 'факт производства'
