9df660f41d3ba16664b23b10ef12b1979d707dd1
Dmitry <da-boyko@mail.ru>
2023-09-17 12:52:07 +0700

    feat: add deleting logic to start command

--- core/bot/handlers/start.py
+++ core/bot/handlers/start.py
@@ -1,16 +1,23 @@
 from asgiref.sync import sync_to_async
 from telegram import Update, InlineKeyboardMarkup
-from telegram.ext import ContextTypes
+from telegram.error import BadRequest
+from telegram.ext import ContextTypes, ConversationHandler
 
 from bot.keyboards import start_keyboard, start_work_keyboard
 from erp.models import User
+from erp.utils import get_user_by_update
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.delete()
     try:
-        user = await sync_to_async(User.objects.get)(chat_id=update.message.chat.id)
-        await context.bot.send_message(
+        await context.chat_data["message"].delete()
+    except (KeyError, BadRequest):
+        pass
+    context.chat_data.clear()
+    try:
+        user = await get_user_by_update(update)
+        mess = await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=f"Здравствуйте, {user.first_name}!\n\n"
                  f"Какую информацию нужно внести?\n",
@@ -18,9 +25,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
     except User.DoesNotExist:
-        await context.bot.send_message(
+        mess = await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="Добрый день, сотрудник!\n\n"
                  "Чтобы пройти регистрацию, нажмите на кнопку ниже!",
             reply_markup=start_keyboard,
         )
+
+    context.chat_data['message'] = mess
+    return ConversationHandler.END
