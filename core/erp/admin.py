ff9c83f08e2d27abfca630916a0733e8cec92345
Dmitry <da-boyko@mail.ru>
2023-09-17 12:52:57 +0700

    feat: add export feature

--- core/erp/admin.py
+++ core/erp/admin.py
@@ -2,12 +2,35 @@ from datetime import datetime
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
+from django.http import HttpRequest
+from import_export.fields import Field
 from rangefilter.filters import DateRangeQuickSelectListFilterBuilder
+from django.contrib.admin import sites
+from import_export.admin import ExportMixin
+from import_export import resources
 
+from erp.enums import WorkArea
 from erp.models import Transfer, User, Production
 
-admin.site.unregister(Group)
+
+class MyAdminSite(admin.AdminSite):
+
+    def get_app_list(self, request: HttpRequest, app_label: str | None = ...):
+        apps = [
+            {
+                'name': 'ссылки',
+                'models': [
+                    {'name': 'Telegram', 'perms': {'View': True}, 'admin_url': 'https://t.me/erp_developing_bot'},
+                ]
+            }
+        ]
+        return apps + super().get_app_list(request)
+
+
+myadmin = MyAdminSite()
+admin.site = myadmin
+sites.site = myadmin
+
 admin.site.site_title = 'Alligator ERP'
 admin.site.index_title = 'Административная панель'
 admin.site.site_header = 'Alligator ERP'
@@ -20,22 +43,37 @@ class MyUserAdmin(UserAdmin):
         'pk',
         'last_name',
         'first_name',
+        'patronymic',
         'telegram_username',
         'area',
     )
 
-    fieldsets = (
-        (
-            'Персональная информация',
-            {
-                'fields': (
-                    'first_name',
-                    'last_name',
-                    'area',
-                )
-            },
-        ),
-        (
+    def get_fieldsets(self, request: HttpRequest, obj=...):
+        fieldsets = (
+            (
+                'Персональная информация',
+                {
+                    'fields': (
+                        'first_name',
+                        'last_name',
+                        'patronymic',
+                        'area',
+                    )
+                },
+            ),
+        )
+
+        if obj.username:
+            return (*fieldsets, (
+                'Настройки',
+                {
+                    'fields': (
+                        'is_staff',
+                        'username',
+                    )
+                }
+            ))
+        return (*fieldsets, (
             'Telegram',
             {
                 'fields': (
@@ -43,27 +81,34 @@ class MyUserAdmin(UserAdmin):
                     'telegram_username',
                 )
             }
-        )
-    )
+        ))
 
-    readonly_fields = (
-        'chat_id',
-        'telegram_username',
-    )
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return [
+                'chat_id',
+                'telegram_username',
+                'username',
+            ]
+        else:
+            return [
+                'chat_id',
+                'telegram_username',
+            ]
+
+    list_filter = ("is_staff",)
 
     class Meta:
         model = User
 
 
-class AbstractAdmin(admin.ModelAdmin):
+class AbstractAdmin(ExportMixin, admin.ModelAdmin):
     list_display = (
         'id',
         'author',
         'date',
         'item_number',
         'amount',
-        'sender',
-        'receiver',
         'comment',
     )
 
@@ -81,22 +126,82 @@ class AbstractAdmin(admin.ModelAdmin):
                 default_end=datetime.now(),
             ),
         ),
-        'sender',
-        'receiver',
     )
     search_fields = ('item_number',)
+
+    def get_export_filename(self, request, queryset, file_format):
+        return f'{self.model.__name__} {datetime.now().strftime("%d.%m.%Y %H-%M")}.xlsx'
+
+    def export_action(self, request, *args, **kwargs):
+        post = request.POST.copy()
+        post['file_format'] = '0'
+        request.POST = post
+        return super().export_action(request, *args, **kwargs)
 
     class Meta:
         abstract = True
 
 
+class AbstractResource(resources.ModelResource):
+    item_number = Field(column_name='Номенклатурный номер', attribute='item_number')
+    amount = Field(column_name='Количество', attribute='amount')
+    date = Field(column_name='Дата', attribute='date')
+
+    class Meta:
+        abstract = True
+        exclude = ('id', 'author', 'comment')
+
+    @staticmethod
+    def dehydrate_date(obj: Transfer):
+        return obj.date.strftime('%d.%m.%Y %H:%M')
+
+
+class TransferResource(AbstractResource):
+    sender = Field(column_name='Отправитель', attribute='sender')
+    receiver = Field(column_name='Получатель', attribute='receiver')
+
+    class Meta:
+        model = Transfer
+        exclude = ('id', 'author', 'comment')
+
+    @staticmethod
+    def dehydrate_sender(obj):
+        return WorkArea.get_label_by_name(obj.sender)
+
+    @staticmethod
+    def dehydrate_receiver(obj):
+        return WorkArea.get_label_by_name(obj.receiver)
+
+
+class ProductionResource(AbstractResource):
+    location = Field(column_name='Цех производства', attribute='location')
+
+    class Meta:
+        model = Production
+        exclude = ('id', 'author', 'comment')
+
+    @staticmethod
+    def dehydrate_location(obj):
+        return WorkArea.get_label_by_name(obj.location)
+
+
 @admin.register(Transfer)
 class TransferAdmin(AbstractAdmin):
+    resource_class = TransferResource
+
+    list_display = (*AbstractAdmin.list_display, 'sender', 'receiver')
+    list_filter = (*AbstractAdmin.list_filter, 'sender', 'receiver')
+
     class Meta:
         model = Transfer
 
 
 @admin.register(Production)
 class ProductionAdmin(AbstractAdmin):
+    resource_class = ProductionResource
+
+    list_display = (*AbstractAdmin.list_display, 'location')
+    list_filter = (*AbstractAdmin.list_filter, 'location')
+
     class Meta:
         model = Production
