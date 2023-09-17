77b2adeffeaceadb810e9b3f16adaa595c5b7c20
Dmitry <da-boyko@mail.ru>
2023-09-17 12:52:38 +0700

    feat: add patronymic name step

--- core/bot/handlers/register.py
+++ core/bot/handlers/register.py
@@ -3,37 +3,54 @@ from telegram import Update
 from telegram.ext import ContextTypes, ConversationHandler, filters, MessageHandler, CallbackQueryHandler
 
 from bot.handlers.start import start_keyboard
-from bot.keyboards import cancel_keyboard, confirm_keyboard, status_check_keyboard
+from bot.keyboards import cancel_keyboard, confirm_keyboard, status_check_keyboard, start_work_keyboard
 from erp.models import User
+from erp.utils import get_user_by_update
 
-FIRST_NAME, LAST_NAME, RESULT = range(3)
+FIRST_NAME, LAST_NAME, PATRONYMIC, RESULT = range(4)
 
 
 async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.callback_query.message
     try:
-        await sync_to_async(User.objects.get)(chat_id=message.chat.id)
-        await message.edit_text(text='Вы уже зарегистрированы')
+        user = await get_user_by_update(update)
+        if user.area:
+            await message.edit_text(
+                text=f"Здравствуйте, {user.first_name}!\n\n"
+                     f"Какую информацию нужно внести?\n",
+                reply_markup=start_work_keyboard
+            )
+        else:
+            await message.edit_text(
+                text='Ждите подтверждения.', reply_markup=status_check_keyboard
+            )
         return ConversationHandler.END
     except User.DoesNotExist:
         await message.edit_text(
             text='Введите фамилию:', reply_markup=cancel_keyboard
         )
         context.chat_data.update({'message': message})
-        return FIRST_NAME
-
-
-async def first_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.delete()
-    context.chat_data.update({'last_name': update.message.text})
-    await context.chat_data['message'].edit_text(text='Введите имя:', reply_markup=cancel_keyboard)
-    return LAST_NAME
+        return LAST_NAME
 
 
 async def last_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.delete()
-    context.chat_data.update({'first_name': update.message.text})
-    full_name = f"{context.chat_data['last_name']} {context.chat_data['first_name']}"
+    context.chat_data.update({'last_name': update.message.text.strip().capitalize()})
+    await context.chat_data['message'].edit_text(text='Введите имя:', reply_markup=cancel_keyboard)
+    return FIRST_NAME
+
+
+async def first_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.delete()
+    context.chat_data.update({'first_name': update.message.text.strip().capitalize()})
+    await context.chat_data['message'].edit_text(text='Введите отчество:', reply_markup=cancel_keyboard)
+    return PATRONYMIC
+
+
+async def patronymic(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.delete()
+    context.chat_data.update({'patronymic': update.message.text.strip().capitalize()})
+    full_name = f"{context.chat_data['last_name']} {context.chat_data['first_name']} {context.chat_data['patronymic']}"
     await context.chat_data['message'].edit_text(
         text=f'{full_name}\n\nВерно?',
         reply_markup=confirm_keyboard,
@@ -45,7 +62,7 @@ async def result(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await sync_to_async(User.objects.create)(
         first_name=context.chat_data['first_name'],
         last_name=context.chat_data['last_name'],
-        username=context.chat_data['message'].chat.id,
+        patronymic=context.chat_data['patronymic'],
         chat_id=context.chat_data['message'].chat.id,
         telegram_username=context.chat_data['message'].chat.username,
     )
@@ -67,8 +84,9 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 register_conv_handler = ConversationHandler(
     entry_points=[CallbackQueryHandler(register, 'register')],
     states={
-        FIRST_NAME: [MessageHandler(filters.Regex(r'^\w*$'), first_name)],
         LAST_NAME: [MessageHandler(filters.Regex(r'^\w*$'), last_name)],
+        FIRST_NAME: [MessageHandler(filters.Regex(r'^\w*$'), first_name)],
+        PATRONYMIC: [MessageHandler(filters.Regex(r'^\w*$'), patronymic)],
         RESULT: [CallbackQueryHandler(result, 'confirm')],
     },
     fallbacks=[CallbackQueryHandler(cancel, 'cancel')],
