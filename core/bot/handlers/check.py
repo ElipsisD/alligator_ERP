35eb70496bc7420ec6503df3abdfac46c933ac5e
Dmitry <da-boyko@mail.ru>
2023-09-17 12:53:43 +0700

    feat: feat check func

--- core/bot/handlers/check.py
+++ core/bot/handlers/check.py
@@ -2,16 +2,23 @@ from asgiref.sync import sync_to_async
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from bot.keyboards import status_check_keyboard, start_transfer_keyboard
+
+from bot.keyboards import status_check_keyboard, start_work_keyboard
 from erp.models import User
+from erp.utils import get_user_by_update
 
 
 async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.callback_query.message
     try:
-        user = await sync_to_async(User.objects.get)(chat_id=message.chat_id)
+        user = await get_user_by_update(update)
         if user.area:
-            await message.edit_text('Давай работать :)', reply_markup=start_transfer_keyboard)
+            await message.edit_text(
+                text=f"Здравствуйте, {user.first_name}!\n\n"
+                     f"Какую информацию нужно внести?\n",
+                reply_markup=start_work_keyboard
+            )
+
         else:
             await message.edit_text(
                 'Подтверждение еще не получено!', reply_markup=status_check_keyboard
